a82762fce757929d5a8dfc543376b076e1a41485
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-06-10 16:04:40 +0200

    call to api successfully created

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Project;
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+        $projects = Project::all();
+        if($projects){
+            return response()->json([
+                'success'=> true,
+                'results'=> $projects
+            ]);
+        }
+        else{
+            return response()->json([
+                'success'=> false,
+                'results'=> 'Project not found'
+            ]);
+        }
+    }
 }
